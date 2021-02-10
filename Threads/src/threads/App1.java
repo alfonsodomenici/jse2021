93881ba065ec493cd0bf04b2035e330d8c977a49
alfonso domenici <alfonso@alfonsodomenici.it>
2021-02-10 07:27:36 +0100

    start

---
+++ Threads/src/threads/App1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threads;
+
+/**
+ *
+ * @author alfonso
+ */
+public class App1 {
+
+    public static void main(String[] args) {
+        new Worker("Worker1").start();
+        new Worker("Worker2").start();
+    }
+}
+
+class Worker extends Thread {
+
+    public Worker(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " " + getName());
+            try {
+                sleep((long) (Math.random() * 1000));
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.println("DONE! " + getName());
+    }
+
+}
---
+++ Threads/src/threads/App2.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threads;
+
+/**
+ *
+ * @author alfonso
+ */
+public class App2 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable worker = new Worker("worker1");
+        Thread t = new Thread(worker);
+        t.start();
+        Thread.sleep(10 * 1000);
+        t.interrupt();
+    }
+}
+
+class Worker implements Runnable {
+
+    private final String name;
+
+    public Worker(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        try {
+            int i = 0;
+            while (true) {
+                Thread.sleep((long) (Math.random() * 1000));
+                System.out.println(i++ + " " + this.name);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("DONE! " + this.name);
+    }
+}
