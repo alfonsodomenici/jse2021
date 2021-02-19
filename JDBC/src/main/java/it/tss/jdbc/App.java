fa0dc2f90daeb647745663c78bca1c781bd9a0a5
Alfonso Domenici <alfonso.domenici@gmail.com>
2021-02-19 10:59:47 +0100

    start

--- JDBC/src/main/java/it/tss/jdbc/App.java
+++ JDBC/src/main/java/it/tss/jdbc/App.java
@@ -7,31 +7,61 @@ package it.tss.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
  *
- * @author tss  
+ * @author tss
  */
 public class App {
-    public static void main(String[] args) throws SQLException {
-        Connection cn = DriverManager
-                .getConnection("jdbc:mariadb://localhost:3306/classicmodels","tss","ghiglieno");
-        System.out.println("connection to db ok....");
-        
-        Statement cmd = cn.createStatement();
-        
-        ResultSet rs = cmd.executeQuery("select customerName, city, state from customers where state like 'NY'");
-    
-        while(rs.next()){
-            System.out.println(String.format("nome: %s , citta: %s, stato: %s", 
-                    rs.getString("customerName"), rs.getString("city"), rs.getString("state")));
-        }
-        
-        rs.close();
-        cmd.close();
-        cn.close();
+
+    private static final String DB_URL = "jdbc:mariadb://localhost:3306/classicmodels";
+    private static final String DB_USR = "tss";
+    private static final String DB_PWD = "ghiglieno";
+    private static final String SQL_QUERY = "select customerName, city, state from customers where state like 'NY'";
+
+    public static void main(String[] args) {
+
+        try (
+                 Connection cn = DriverManager
+                        .getConnection(App.DB_URL, App.DB_USR, App.DB_PWD);  
+                Statement cmd = cn.createStatement();  
+                ResultSet rs = cmd.executeQuery(App.SQL_QUERY);  
+                PreparedStatement insert = cn.prepareStatement("insert into productlines (productLine,textDescription ) values (?, ?)");
+                PreparedStatement delete = cn.prepareStatement("delete from productlines where productLine = ?");
+                ) {
+                    try {
+                        cn.setAutoCommit(false);
+                        for (int i = 1; i <= 1000; i++) {
+                            String key = "prova" + i;
+                            //insert.setString(1, key);
+                            //insert.setString(2, "prova insert");
+                            //insert.executeUpdate();
+                            
+                            delete.setString(1, key);
+                            delete.executeUpdate();
+                            
+                            //cmd.executeUpdate("insert into productlines (productLine,textDescription ) values ('" + key + "', 'prova insert')");
+                            //cmd.executeUpdate("delete from productlines where productLine = '" + key + "'");
+                        }
+                        
+                        cn.commit();
+                    } catch (SQLException e) {
+                        cn.rollback();
+                        System.out.println(e.getMessage());
+                    }
+                    while (rs.next()) {
+                        System.out.println(String.format("nome: %s , citta: %s, stato: %s",
+                                rs.getString("customerName"), rs.getString("city"), rs.getString("state")));
+                    }
+
+                } catch (SQLException e) {
+
+                    System.out.println(e.getMessage());
+                }
     }
 }
